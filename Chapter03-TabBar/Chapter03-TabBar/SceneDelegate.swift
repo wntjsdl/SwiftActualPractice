e2ca81f5a2f2a0b4ac9982e679c001e8a3d5a41c
Jusun Kang <wntjs0305@gmail.com>
2021-08-01 01:50:12 +0900

    Chapter03 - 3.1.6 완료

--- Chapter03-TabBar/Chapter03-TabBar/SceneDelegate.swift
+++ Chapter03-TabBar/Chapter03-TabBar/SceneDelegate.swift
@@ -17,6 +17,59 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // 1. 루트 뷰 컨트롤러를 UITabBarController로 캐스팅한다.
+        if let tbC = self.window?.rootViewController as? UITabBarController {
+            // 2. 탭 바에서 탭 바 아이템 배열을 가져온다.
+            if let tbItems = tbC.tabBar.items {
+                // 3. 탭 바 아이템에 커스텀 이미지를 등록한다.
+//                tbItems[0].image = UIImage(named: "calendar")
+//                tbItems[1].image = UIImage(named: "file-tree")
+//                tbItems[2].image = UIImage(named: "photo")
+                tbItems[0].image = UIImage(named: "designbump")?.withRenderingMode(.alwaysOriginal)
+                tbItems[1].image = UIImage(named: "rss")?.withRenderingMode(.alwaysOriginal)
+                tbItems[2].image = UIImage(named: "facebook")?.withRenderingMode(.alwaysOriginal)
+                
+                // 4. 탭 바 아이템에 타이틀을 설정한다.
+                tbItems[0].title = "calendar"
+                tbItems[1].title = "file"
+                tbItems[2].title = "photo"
+                
+                /*
+                // 탭 바 아이템 전체를 순회하면서 selectedImage 속성에 이미지를 설정한다.
+                for tbItem in tbItems {
+                    let image = UIImage(named: "checkmark")?.withRenderingMode(.alwaysOriginal)
+                    tbItem.selectedImage = image
+                    
+                    // 탭 바 아이템별 텍스트 색상 속성을 설정한다.
+                    tbItem.setTitleTextAttributes([.foregroundColor: UIColor.gray], for: .disabled)
+                    tbItem.setTitleTextAttributes([.foregroundColor: UIColor.red], for: .selected)
+                    
+                    // 전체 아이템의 폰트 크기를 설정한다.
+                    tbItem.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 15)], for: .normal)
+                }
+                */
+                
+                // 외형 프록시 객체를 이용하여 아이템의 타이틀 색상과 폰트 크기를 설정한다.
+                let tbItemProxy = UITabBarItem.appearance()
+                tbItemProxy.setTitleTextAttributes([.foregroundColor: UIColor.red], for: .selected)
+                tbItemProxy.setTitleTextAttributes([.foregroundColor: UIColor.gray], for: .disabled)
+                tbItemProxy.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 15)], for: .normal)
+                
+                let tbProxy = UITabBar.appearance()
+                // 5) 활성화된 탭 바 아이템의 이미지 색상을 변경한다.
+                tbProxy.tintColor = UIColor.white
+                // 6) 탭 바에 배경 이미지를 설정한다.
+                tbProxy.backgroundImage = UIImage(named: "menu-bg-mini")
+            }
+            
+            // 5. 탭 바 아이템의 이미지 색상을 변경한다.
+//            tbC.tabBar.tintColor = .white // 선택된 탭 바 아이템의 색상
+//            tbC.tabBar.unselectedItemTintColor = .gray // 선택되지 않은 나머지 탭 바 아이템의 색상
+            
+            // 6. 탭 바에 배경 이미지를 설정한다.
+//            tbC.tabBar.backgroundImage = UIImage(named: "menubar-bg-mini")
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
+++ Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
@@ -29,12 +29,18 @@ class ViewController: UIViewController {
         
         // 5) 수퍼 뷰에 추가
         self.view.addSubview(title)
-        
-        // 탭 바 아이템에 커스텀 이미지를 등록하고 탭 이름을 입력한다.
-        self.tabBarItem.image = UIImage(named: "calendar.png")
-        self.tabBarItem.title = "Calendar"
     }
 
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let tabBar = self.tabBarController?.tabBar
+//        tabBar?.isHidden = (tabBar?.isHidden == true) ? false : true
+        
+        UIView.animate(withDuration: TimeInterval(0.15)) {
+            // alpha 값이 0 이면 1로, 1이면 0으로 바꾸어 준다.
+            // 호출될 때마다 점점 투명해졌다가 점점 진해질 것이다.
+            tabBar?.alpha = ( tabBar?.alpha == 0 ? 1 : 0 )
+        }
+    }
 
 }
 
