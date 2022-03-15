224dc6dca0e39811976bd529b2f9c44ff6e7aedd
wntjsdl <koreacenterappteamsd@gmail.com>
2022-03-15 12:24:41 +0900

    4.2.4 SideBarDIY

---
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/FrontViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FrontViewController.swift
+//  Chapter04-SideBarDIY
+//
+//  Created by KJS on 2022/03/15.
+//
+
+import UIKit
+
+class FrontViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/SideBarViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SideBarViewController.swift
+//  Chapter04-SideBarDIY
+//
+//  Created by KJS on 2022/03/15.
+//
+
+import UIKit
+
+class SideBarViewController: UITableViewController {
+    
+    // 메뉴 제목 배열
+    let titles = [
+        "메뉴 01",
+        "메뉴 02",
+        "메뉴 03",
+        "메뉴 04",
+        "메뉴 05"
+    ]
+    
+    // 메뉴 아이콘 배열
+    let icons = [
+        UIImage(named: "icon01.png"),
+        UIImage(named: "icon02.png"),
+        UIImage(named: "icon03.png"),
+        UIImage(named: "icon04.png"),
+        UIImage(named: "icon05.png")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let accountLabel = UILabel()
+        accountLabel.frame = CGRect(x: 10, y: 30, width: self.view.frame.width, height: 30)
+        
+        accountLabel.text = "sqlpro@naver.com"
+        accountLabel.textColor = .white
+        accountLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        
+        let v = UIView()
+        v.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70)
+        v.backgroundColor = .brown
+        v.addSubview(accountLabel)
+        
+        self.tableView.tableHeaderView = v
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.titles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 재사용큐 사용
+        let id = "menucell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: id) ?? UITableViewCell(style: .default, reuseIdentifier: id)
+        
+        // 재사용큐 미 사용
+//        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = self.titles[indexPath.row]
+        cell.imageView?.image = self.icons[indexPath.row]
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        
+        return cell
+    }
+    
+}
